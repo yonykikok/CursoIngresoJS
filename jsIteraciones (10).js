6081859216b0371075c560a5f3239ea9fae51bef
Haedo Jonathan <36281500+yonykikok@users.noreply.github.com>
2018-03-03 23:08:12 -0300

    while 10

--- jsIteraciones (10).js
+++ jsIteraciones (10).js
@@ -2,83 +2,61 @@ function Mostrar()
 {
 /*Enunciado:
 Al presionar el botón pedir números hasta que el usuario quiera, mostar:
-1-Suma de los negativos. 2-Suma de los positivos. 3-Cantidad de positivos. 
-4-Cantidad de negativos. 5-Cantidad de ceros. 6-Cantidad de números pares. 
-7-Promedio de positivos. 8-Promedios de negativos. 
+1-Suma de los negativos. 
+2-Suma de los positivos. 
+3-Cantidad de positivos. 
+4-Cantidad de negativos. 
+5-Cantidad de ceros. 
+6-Cantidad de números pares. 
+7-Promedio de positivos. 
+8-Promedios de negativos. 
 9-Diferencia entre positivos y negativos, (positvos-negativos).*/
-	var contador=0;
-	//declarar contadores y variables 
-	var numeros;
-	var seguro;
-	var sumapositivo=0;
-	var sumanegativo=0;
-	var cantidadpositivos=0;
-	var cantidadnegativos=0;
-	var cantidadcero=0;
-	var cantidadnumerospares=0;
-	var promediopositivo;
-	var promedionegativo;
-	var diferenciapositivonegativo;// (positivos-negativos)
-
-	var respuesta="si";
-
-	while(respuesta!="no")
-	{
-		numeros=prompt("ingrese los numeros");
-		if (numeros==null) 
-			{
-				seguro=prompt("Para salir ingrese 'si'");
-				if (seguro=="si") 
-					{
-						document.write("<br>"+"suma de positivo "+sumapositivo);
-						document.write("<br>"+"suma de negativo "+sumanegativo);
-						document.write("<br>"+"cantidad de negativos "+cantidadnegativos);
-						document.write("<br>"+"cantidad de positivos "+cantidadpositivos);
-						document.write("<br>"+"cantidad de ceros "+cantidadcero);
-						document.write("<br>"+"cantidad de numeros pares "+cantidadnumerospares);
-						document.write("<br>"+"promedio negativo "+promedionegativo);
-						document.write("<br>"+"promedio positivo "+promediopositivo);
-						document.write("<br>"+"diferencia positivo-negativo "+diferenciapositivonegativo);
-						break;
-
-					}	
-					else
-						alert("Bueno, Sigamos");
-
-			}
-			numeros=parseInt(numeros);
-
-			if (numeros<0) 
-				{
-				sumanegativo=sumanegativo+numeros;
-				cantidadnegativos++;
-
-				}
-				if (numeros>0) 
-					{
-						sumapositivo=sumapositivo+numeros;
-						cantidadpositivos++;
-					}
-					if (numeros==0) 
-						{
-							cantidadcero++;
-						}
-						
-						promediopositivo=cantidadpositivos/numeros;
-						promedionegativo=cantidadnegativos/numeros;	
-						diferenciapositivonegativo=sumapositivo+ sumanegativo;	// no me da bien..
-						if (numeros % 2 ==0) 
-							{
-								cantidadnumerospares++;
-							}
-			if (isNaN(numeros)) 
-			{
-				alert("ingrese numeros por favor");
-			}
-
 	
-	}
+	var entrada="si"
+	var numeros;
 
+	var contadorsuma=0;
+	var suma=0;
+
+	var contadorNegativo=0;
+	var sumaNegativos=0;
+	var contadorCero=0;
+	var contadorPar=0;
+	var promediopositivos;
+	var promedioNegativos;
+	var diferencia;
+
+
+	while(entrada=="si")
+	{
+		numeros=prompt("ingrese un numero");
+		if (numeros==null) {break;}
+		numeros=parseInt(numeros);
+
+		if (numeros<0) 
+			{
+				contadorNegativo++;
+				sumaNegativos=sumaNegativos+numeros;
+			}
+			if (numeros>0) 
+				{
+					contadorsuma++;
+					suma=suma+numeros;
+				}
+				if (numeros==0) 
+					{
+						contadorCero++;
+					}
+					if (numeros%2==0&&numeros!=0) 
+					{
+						contadorPar++;
+					}
+					diferencia=suma+sumaNegativos;
+					promediopositivos=suma/contadorsuma;
+					promedioNegativos=sumaNegativos/contadorNegativo;
+	}	
+	document.write("diferencia entre positivos y negativos "+diferencia+"<br>"+"contador pares "+contadorPar+"<br>"+"contador de '0' "+contadorCero+"<br>"+"suma de negativos "+sumaNegativos+"<br>"+ "suma de positivos "+suma+"<br>"+"contador de suma "+contadorsuma+"<br>"+"contador de negativos "+contadorNegativo+"<br>"+"promedio de positivos "+promediopositivos+"<br>"+"promedio de negativos "+promedioNegativos);
+	
 
 
 
